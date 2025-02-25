a7921ec93642d9c765f9849b919250b42505917c
ViniciusBrunesCJ <viniciusbrunes2@gmail.com>
2025-02-24 21:30:21 -0300

    Add files via upload

---
+++ Perfil.java.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nome: Vinicius Brunes Barros");
+        System.out.println("Idade: 19 Anos");
+        System.out.println("Matar porcos");
+    }
+}
